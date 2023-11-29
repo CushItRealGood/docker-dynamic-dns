b8a224a300a55989fb2459d73ea1de9edf145713
madereddy <49539048+madereddy@users.noreply.github.com>
2023-11-29 10:19:16 -0500

    Remove source of error by splitting if.

--- no-ip.sh
+++ no-ip.sh
@@ -118,7 +118,12 @@ if [ -z "$DOMAINS" ]; then
 
 		RESULT=$(wget --no-check-certificate -qO- $AUTHHEADER $USERAGENT $NOIPURL)
 
-		if [ "$RESULT" != 'good' ] && [ "$RESULT" != 'nochg' ]; then
+		if [ "$RESULT" != 'good' ]; then
+			echo "DNS update issues encountered."
+			exit 1
+		fi
+		
+		if [ "$RESULT" != 'nochg' ]; then
 			echo "DNS update issues encountered."
 			exit 1
 		fi
